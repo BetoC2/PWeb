1d8c76141eaed2dba525a1af9413a2821bc30c6f
RENTERIA CAMACHO <alberto.renteriac@iteso.mx>
2023-10-16 08:28:03 -0600

    Sesion 19

---
+++ S19/script.js
@@ -0,0 +1,7 @@
+function hola(value){
+    for (let i = 1; i <= 5; i++) {
+        let id = "div_" + i;
+        element = document.getElementById(id);
+        element.innerText = value == 0 ? (i%2 == 0 ? "Pares activos": "Disabled"): (i%2 != 0? "Impares activos": "Disabled") 
+    }
+}   
